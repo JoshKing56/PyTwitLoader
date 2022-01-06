85af797481d3925052724bd192285e02c87e6c01
JoshKing56 <joshuaiking56@gmail.com>
2022-01-06 02:03:46 -0500

    Working request, needs refactoring

---
+++ pytwitloader/configuration.py
@@ -0,0 +1,24 @@
+import logging
+import sys
+import json
+import os
+
+logger = logging.getLogger("main")
+
+def get_twitter_keys():
+    config = {} 
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        with open(filename, "r") as config_file:
+            config = json.load(config_file)
+    else:
+        env_vars = os.environ
+        try:
+                config["TWITTER_CONSUMER_KEY"] = env_vars["TWITTER_CONSUMER_KEY"]
+                config["TWITTER_CONSUMER_SECRET"] = env_vars["TWITTER_CONSUMER_SECRET"]
+                config["TWITTER_ACCESS_KEY"]= env_vars["TWITTER_ACCESS_KEY"]
+                config["TWITTER_ACCESS_SECRET"]= env_vars["TWITTER_ACCESS_SECRET"]
+        except KeyError:
+            logger.exception("No config file passed, and no environment variables found")
+            sys.exit()
+    return config
--- pytwitloader/main.py
+++ pytwitloader/main.py
@@ -2,49 +2,42 @@ import json
 import logging
 import os
 import sys
+import configuration
+# import tweepy
+import requests
 
-import tweepy
-
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("main")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('output.log')
-formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+file_handler = logging.FileHandler("output.log")
+formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
 
-def get_twitter_keys():
-    if len(sys.argv) > 1:
-        filename = sys.argv[1]
-        with open(filename, "r") as config_file:
-            config = json.load(config_file)
-        return config["consumer_key"], config["consumer_secret"]
-    else:
-        env_vars = os.environ
-        try:
-            return env_vars["TWITTER_CONSUMER_KEY"], env_vars["TWITTER_CONSUMER_SECRET"]
-        except KeyError:
-            logger.error("No config file passed, and no environment variables found") 
-            # print("testing")
-            sys.exit()
-
+    config = configuration.get_twitter_keys()
+    bearer_token = config["TWITTER_BEARER_TOKEN"]
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    # r.headers["User-Agent"] = "v2LikedTweetsPython"
+    return r
 
 def main():
-    consumer_key, consumer_secret = get_twitter_keys()
-    print(f"Key: {consumer_key},\nSecret :{consumer_secret}")
-    # auth = tweepy.OAuthHandler(config["tw"], consumer_secret)
-    # token = session.get('request_token')
-    # session.delete('request_token')
-    # auth.request_token = { 'oauth_token' : token,
-    #                         'oauth_token_secret' : verifier }
-
-
-    # try:
-    #     auth.get_access_token(verifier)
-    # except tweepy.TweepError:
-    #     print('Error! Failed to get access token.')
-
+    id = "1094960355384745985"
+    url = f"https://api.twitter.com/2/users/{id}/liked_tweets" 
+    # tweet_fields = "tweet.fields=lang,author_id"
+    response = requests.request("GET", url, auth=bearer_oauth)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    # print(response.json)
+    print(json.dumps(response.json(), indent=4, sort_keys=True))
 
 if __name__ == "__main__":
     main()
-
